5884cdf38e3fb851f8f3c85d67369cd0c2b3132e
SeanDaBlack <seandablack@gmail.com>
2025-06-04 12:35:26 -0700

    new apps and new tracks

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import time
 import os
 from resume_faker import make_resume
-from util_functions import start_driver, generate_fake_identity, fill_form
+from util_functions import start_driver, generate_fake_identity, fill_form_app1, fill_form_app2, check_if_app_exists, start_driver_local, fill_form_all
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,6 +12,30 @@ fake = Faker()
 
 
 
+urls = ["https://recruiting.paylocity.com/Recruiting/Jobs/Apply/3173329","https://recruiting.paylocity.com/Recruiting/Jobs/Apply/3223576", "https://recruiting.paylocity.com/Recruiting/Jobs/Apply/2978847", "https://recruiting.paylocity.com/Recruiting/Jobs/Apply/3292538"]
+
+
+
+def get_random_url(driver):
+    """Select a random URL from the list."""
+    
+    while True:
+        url = random.choice(urls)
+        try:
+            print("checking if app exists...")
+            if not check_if_app_exists(driver, url):
+                print("Application does not exist, selecting a new URL...")
+                continue
+                         
+            return urls.index(url)
+        except Exception as e:
+            print(f"Error accessing URL: {e}")
+            time.sleep(2)
+            
+        
+    
+    
+    return random.choice(urls)
 
 
 if __name__ == "__main__":
@@ -22,6 +46,7 @@ if __name__ == "__main__":
 
             print("Starting a new iteration...")
         
+            # driver = start_driver_local()
             driver = start_driver()
             
             print("Driver started successfully.")
@@ -34,8 +59,32 @@ if __name__ == "__main__":
             resume = make_resume(name, fake_identity['email'], f"{name}.pdf")
 
             print("Resume generated successfully.")
+            # Select random form of the 2
+
+            # match get_random_url(driver):
+            #     case 0:
+            #         print("Filling out form for Application 1...")
+            #         fill_form_app1(driver, fake_identity)
+            #     case 1:
+            #         print("Filling out form for Application 2...")
+            #         fill_form_app2(driver, fake_identity)
+            #     case 2:
+            #         print("Filling out form for Application 3...")
+            #         fill_form_all(driver, fake_identity, urls[2])
+            #     case _:
+            #         print("No valid application found, retrying...")
+            #         continue
+                        
+                    
             print("Filling out the form...")
-            fill_form(driver, fake_identity)
+            
+            url = get_random_url(driver)
+            fill_form_all(driver, fake_identity, urls[url], url)
+
+
+            
+            # print("Filling out the form...")
+            # fill_form(driver, fake_identity)
             
             print("Form filled successfully.")
             # Wait for a few seconds before the next iteration
